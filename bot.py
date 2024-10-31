f00c00e99473555b1f496b1b926cb079aecd4731
iambasileia <basileiamailbox@gmail.com>
2024-10-31 15:32:03 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ watchlist = {}
 exceptionlist = []
 mention_dict = loadMentions()
 keywordsFile = loadKeywords()
-db = postgres.Postgres(url=os.environ.get('LOCALHOST'), dbname=os.environ.get('DBNAME'), user=os.environ.get('DBUSER'), password=os.environ.get('DBPASSWORD'))
+db = postgres.Postgres(url=os.environ.get('LOCALHOST'), user=os.environ.get('DBUSER'), password=os.environ.get('DBPASSWORD'))
 db.run("CREATE TABLE IF NOT EXISTS forbidden (words text)")
 db.run("CREATE TABLE IF NOT EXISTS vile (words text)")
 db.run("CREATE TABLE IF NOT EXISTS automute (words text)")
