48e9c9a656c953580d11a88c31716314842775da
iambasileia <basileiamailbox@gmail.com>
2024-10-31 23:49:45 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ COMMANDCHNNUM = int(os.environ.get('COMMANDCHN'))
 REPORTCHNNUM = int(os.environ.get('REPORTCHN'))
 REMOVECHNNUM = int(os.environ.get('REMOVECHN'))
 LOGCHNNUM = int(os.environ.get('LOGCHN'))
-DATABASEURL = os.environ.get('URL')
+DATABASE_URL = DataSource(os.environ.get('URL'))
 
 def loadMentions():
     tmpm = {}
@@ -71,8 +71,8 @@ watchlist = {}
 exceptionlist = []
 mention_dict = loadMentions()
 keywordsFile = loadKeywords()
-conn = psycopg2.connect(host = DATABASEURL, user = os.environ.get('DBUSER'), password = os.environ.get('DBPASSWORD'), database = os.environ.get('DBNAME'))
-db = postgres.Postgres(url= DATABASEURL)
+conn = psycopg2.connect(host = DATABASE_URL, user = os.environ.get('DBUSER'), password = os.environ.get('DBPASSWORD'), database = os.environ.get('DBNAME'))
+db = postgres.Postgres(url= DATABASE_URL)
 db.run("CREATE TABLE IF NOT EXISTS forbidden (words text)")
 db.run("CREATE TABLE IF NOT EXISTS vile (words text)")
 db.run("CREATE TABLE IF NOT EXISTS automute (words text)")
